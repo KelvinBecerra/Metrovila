75fb353fe61633e26d4656e9ced4e605fc04e46a
KelvinBecerra <urielbecerra39@gmail.com>
2025-03-07 12:50:53 -0400

    otro cambio mas :(

--- src/router.jsx
+++ src/router.jsx
@@ -1,47 +1,16 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter,Routes,Route } from "react-router-dom";
 import Moldepages from "./layout/Moldepages";
 import Formulariomolde from "./layout/Formulariomolde";
 import Home from "./pages/Home";
-import Formulario from "./pages/Formulario"
+import Formulario from "./pages/Formulario";
 import Inisesion from "./pages/Inisesion"
 import Rutas from "./pages/Rutas";
 
-export const router = createBrowserRouter([
-    {
-        path:"/",
-        element: <Moldepages/>,
-        /*aqui se le esta asignando el diseno de pagina principal
-        a las paginas que esten como hijos
-        las cuales deberian ser las de inicio
-         */
-        children: [
-            {
-                path:"/",
-                element: <Home/>
-            },
-            {
-                path:"/TuSesion",
-                element: <Inisesion/>
-            }
-        ]
-    },
-    {/**aqui se le esta asignando el diseno a la pagina formulario */
-        path:"/formulario",
-        element: <Formulariomolde/>,
-        children:[
-            {
-                path:"/formulario",
-                element: <Formulario/>
-            }
-        ]
-       
-    },
-    {
-        path:"/iniciosesion",
-        element: <Inisesion/>
-    },
-    {
-        path:"/rutas",
-        element: <Rutas/>
-    }
-])
+export const router = createBrowserRouter(
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/formulario" element={<Formulario/>}/>
+        <Route path="/inisesion" element={<Inisesion/>}/>
+        <Route path="/rutas" element={<Rutas/>}/>
+    </Routes>
+);
