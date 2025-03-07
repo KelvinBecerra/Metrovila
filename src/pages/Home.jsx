d3836f4a07f8f29cf594323df2a83c6a14a231d8
KelvinBecerra <urielbecerra39@gmail.com>
2025-03-07 13:29:53 -0400

    cambio x100001

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,8 @@ import Mision from '../components/Mision'
 function Home() {
   return (
     <>
-      <header>
-            <div class="header-buttons">
-              <a href='/formulario'>
-                <button class="btn ">Registrar</button>
-              </a>
-              <a href="/iniciosesion">
-              <button class="btn ">Iniciar Sesion</button>
-      
-              </a>
-            </div>
-            <div class="logo">
-              <img id="logo" src={logo} alt="Logo" />{/*Aqui es donde se esta importando el logo*/}
-            </div>
-          </header>
+      <Bienvenida/>
+      <Mision/>
     </>
   )
 }
