055870289b7d6ab875d188cb6676a70957b4c610
KelvinBecerra <urielbecerra39@gmail.com>
2025-03-07 13:24:30 -0400

    cambiox1000000

--- src/router.jsx
+++ src/router.jsx
@@ -1,20 +1,47 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Moldepages from './layout/Moldepages';
-import Formulariomolde from './layout/Formulariomolde';
-import Home from './pages/Home';
-import Formulario from './pages/Formulario';
-import Inisesion from './pages/Inisesion';
-import Rutas from './pages/Rutas';
+import { createBrowserRouter } from "react-router-dom";
+import Moldepages from "./layout/Moldepages";
+import Formulariomolde from "./layout/Formulariomolde";
+import Home from "./pages/Home";
+import Formulario from "./pages/Formulario";
+import Inisesion from "./pages/Inisesion"
+import Rutas from "./pages/Rutas";
 
-const routes = [
-  { path: '/', element: <Home /> },
-  { path: '/formulario', element: <Formulario /> },
-  { path: '/inisesion', element: <Inisesion /> },
-  { path: '/rutas', element: <Rutas /> }
-];
-
-const router = createBrowserRouter(routes);
-
-export default function App() {
-  return <RouterProvider router={router} />;
-}
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <Moldepages/>,
+        /*aqui se le esta asignando el diseno de pagina principal
+        a las paginas que esten como hijos
+        las cuales deberian ser las de inicio
+         */
+        children: [
+            {
+                path:"/",
+                element: <Home/>
+            },
+            {
+                path:"/TuSesion",
+                element: <Inisesion/>
+            }
+        ]
+    },
+    {/**aqui se le esta asignando el diseno a la pagina formulario */
+        path:"/formulario",
+        element: <Formulariomolde/>,
+        children:[
+            {
+                path:"/formulario",
+                element: <Formulario/>
+            }
+        ]
+       
+    },
+    {
+        path:"/iniciosesion",
+        element: <Inisesion/>
+    },
+    {
+        path:"/rutas",
+        element: <Rutas/>
+    }
+])
