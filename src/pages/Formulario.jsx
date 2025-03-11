69f927af0595fb88d05165e3c860f8f17630c768
chrisx1610 <christian.goncalves@correo.unimet.edu.ve>
2025-03-10 20:45:17 -0400

    Revert "Resolver"
    This reverts commit 2d6eb4f47cb7fccc775d2617fdc8f8a4dcb888dc, reversing
    changes made to 3ebe835b21ab637aa6de464f08cd48e0e0137aaa.

--- src/pages/Formulario.jsx
+++ src/pages/Formulario.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import DatosInput from '../components/DatosInput';
+import React from 'react'
+import DatosInput from '../components/DatosInput'
+
 
 function Formulario() {
   return (
-    <div className="formulario-container">
-      <DatosInput />
-      <div className="botones-container">
-        <button className='botoncitosini'>Ingresar</button>
-        <a href="/">
-          <button className='botoncitosini'>home</button>
-        </a>
-      </div>
+    <div>
+      
+      <DatosInput/>
+      <button>Ingresar</button>
+      <a href="/">
+        <button>home</button>
+      </a>
+      
     </div>
-  );
+  )
 }
 
-export default Formulario;
+export default Formulario
