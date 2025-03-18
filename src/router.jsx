c0a85e0785db5c8a5f1e8b3f1dd4a0991be8e341
César Gascón <c.gascon@correo.unimet.edu.ve>
2025-03-18 18:50:36 -0400

    Update (es más, me mato 3 veces)

--- src/router.jsx
+++ src/router.jsx
@@ -3,50 +3,45 @@ import Moldepages from "./layout/Moldepages";
 import Formulariomolde from "./layout/Formulariomolde";
 import Home from "./pages/Home";
 import Formulario from "./pages/Formulario";
-import Inisesion from "./pages/Inisesion"
+import Inisesion from "./pages/Inisesion";
 import Rutas from "./pages/Rutas";
+import RutaDetalle from "./pages/RutaDetalle";
 
-import RutaDetalle from "./pages/RutaDetalle"; //NUEVO 
-
-//asi fue como maneje las rutas en el proyecto
-//hay una manera mas facil segun he visto, pero me parecio mas comoda esta
-//
 export const router = createBrowserRouter([
-    {
-        path:"/",
-        element: <Moldepages/>,
-        /*aqui se le esta asignando el diseno de pagina principal
-        a las paginas que esten como hijos
-        las cuales deberian ser las de inicio
-         */
-        children: [
-            {
-                path:"/",
-                element: <Home/>
-            },
-            {
-                path:"/TuSesion",
-                element: <Inisesion/>
-            }
-        ]
-    },
-    {/**aqui se le esta asignando el diseno a la pagina formulario */
-        path:"/formulario",
-        element: <Formulariomolde/>,
-        children:[
-            {
-                path:"/formulario",
-                element: <Formulario/>
-            }
-        ]
-       
-    },
-    {
-        path:"/iniciosesion",
-        element: <Inisesion/>
-    },
-    {
-        path: "/rutas/:rutaId",
-        element: <RutaDetalle />, //(NUEVO)
-    }
-])
+  {
+    path: "/",
+    element: <Moldepages />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/TuSesion",
+        element: <Inisesion />,
+      },
+    ],
+  },
+  {
+    path: "/formulario",
+    element: <Formulariomolde />,
+    children: [
+      {
+        path: "/formulario",
+        element: <Formulario />,
+      },
+    ],
+  },
+  {
+    path: "/iniciosesion",
+    element: <Inisesion />,
+  },
+  {
+    path: "/rutas",
+    element: <Rutas />, // Ruta principal para ver todas las rutas.
+  },
+  {
+    path: "/rutas/:rutaId",
+    element: <RutaDetalle />, // Ruta para detalles individuales.
+  },
+]);
