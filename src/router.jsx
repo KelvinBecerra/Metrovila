8cc4fd9d1bf5d09aad3762ebf65031b5671832d8
Javier <162258018+javiercillx@users.noreply.github.com>
2025-03-23 15:59:32 -0400

    auxilio

--- src/router.jsx
+++ src/router.jsx
@@ -5,43 +5,43 @@ import Home from "./pages/Home";
 import Formulario from "./pages/Formulario";
 import Inisesion from "./pages/Inisesion";
 import Rutas from "./pages/Rutas";
-import Pago from "./pages/Pago"; // Importar Pago
+import Pago from "./pages/Pago"; // Ruta para el componente Pago
 
 export const router = createBrowserRouter([
-    {
-        path:"/",
-        element: <Moldepages/>,
-        children: [
-            {
-                path:"/",
-                element: <Home/>
-            },
-            {
-                path:"/TuSesion",
-                element: <Inisesion/>
-            }
-        ]
-    },
-    {
-        path:"/formulario",
-        element: <Formulariomolde/>,
-        children:[
-            {
-                path:"/formulario",
-                element: <Formulario/>
-            }
-        ]
-    },
-    {
-        path:"/iniciosesion",
-        element: <Inisesion/>
-    },
-    {
-        path:"/rutas",
-        element: <Rutas/>
-    },
-    {
-        path:"/pago", // Ruta para el componente Pago
-        element: <Pago/>
-    }
+  {
+    path: "/",
+    element: <Moldepages />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/TuSesion",
+        element: <Inisesion />,
+      },
+    ],
+  },
+  {
+    path: "/formulario",
+    element: <Formulariomolde />,
+    children: [
+      {
+        path: "/formulario",
+        element: <Formulario />,
+      },
+    ],
+  },
+  {
+    path: "/iniciosesion",
+    element: <Inisesion />,
+  },
+  {
+    path: "/rutas",
+    element: <Rutas />,
+  },
+  {
+    path: "/pago", // Ruta para el componente Pago
+    element: <Pago />,
+  },
 ]);
